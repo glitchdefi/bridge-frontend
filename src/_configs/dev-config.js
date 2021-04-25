6a8593ff4a5c01c4dba40f996c4567f80add3a50
Thoai Nguyen <nguyenthoai06011990@gmail.com>
2021-04-25 21:30:43 +0700

    update config change address

--- src/_configs/dev-config.js
+++ src/_configs/dev-config.js
@@ -2,12 +2,12 @@ module.exports = {
     BLOCKCHAIN_NETWORK: 'TESTNET',
     BSC_EXPLORER: 'https://testnet.bscscan.com',
     BSC_NODE_URL: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
-    BSC_GLITCH_ADDRESS: '0x7fa4fc16bfe93fd0fcadb5112e178e17bed1ce06',
-    BSC_BRIDGE_CONTRACT_ADDRESS: '0xfe3d88b86e42f9ad883e37cac0b2708894b9d9e9',
+    BSC_GLITCH_ADDRESS: '0xaf2c60d15cee57ddba603ba6b3748a9c8661d4a7',
+    BSC_BRIDGE_CONTRACT_ADDRESS: '0x728e02bb39fe0dcec8e45cc3f900ea3a53e419c0',
     ETH_EXPLORER: 'https://ropsten.etherscan.io',
     ETH_NODE_URL:
         'https://ropsten.infura.io/v3/f2473914890349138c8b03e3ef79d165/',
-    ETH_GLITCH_ADDRESS: '0x5aa682db6cbe27e0ca0ee2a31948e6000222606b',
-    ETH_BRIDGE_CONTRACT_ADDRESS: '0x4e6bffa285c8a64df224aab632a75990508b1763',
+    ETH_GLITCH_ADDRESS: '0x674838d4924b3c4dd4900a1be676109cc729f077',
+    ETH_BRIDGE_CONTRACT_ADDRESS: '0x922fb812c9831cfa5ac69f55eca479d738b899b6',
     BACK_END_URL: 'https://uat-bridge.glitch.finance'
 };
