28fd01e6b56c6c9b40db380a8d7d7385a82dc159
Thoai Nguyen <nguyenthoai06011990@gmail.com>
2021-04-29 11:20:09 +0700

    update

--- src/_configs/dev-config.js
+++ src/_configs/dev-config.js
@@ -5,8 +5,8 @@ module.exports = {
     BSC_GLITCH_ADDRESS: '0xC29c28Ef6eC433076C5e0BCbAFcC033537b48B0b',
     BSC_BRIDGE_CONTRACT_ADDRESS: '0x2700d71C5FCfd5109f6997Aaf6A6e1B47ED0E717',
     ETH_EXPLORER: 'https://ropsten.etherscan.io',
-    ETH_NODE_URL:
-        'https://ropsten.infura.io/v3/f2473914890349138c8b03e3ef79d165/',
+    // ETH_NODE_URL:
+    //     'https://ropsten.infura.io/v3/f2473914890349138c8b03e3ef79d165/',
     ETH_GLITCH_ADDRESS: '0xB176b2f01487a1A452e058f2b386a6eD30D38128',
     ETH_BRIDGE_CONTRACT_ADDRESS: '0xfeA109179cC904C5CBdD4E4d569e222f60487340',
     BACK_END_URL: 'https://uat-bridge.glitch.finance'
