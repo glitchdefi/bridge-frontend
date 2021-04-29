6526bb906071db32abc0b6e066b294ff308571f9
Thoai Nguyen <nguyenthoai06011990@gmail.com>
2021-04-29 13:12:05 +0700

    update config

--- src/_configs/prod-config.js
+++ src/_configs/prod-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    BLOCKCHAIN_NETWORK: 'TESTNET',
-    BSC_EXPLORER: 'https://testnet.bscscan.com',
+    BLOCKCHAIN_NETWORK: 'TESTNET', // MAINNET
+    BSC_EXPLORER: 'https://testnet.bscscan.com',  //https://bscscan.com
     BSC_GLITCH_ADDRESS: '0xC29c28Ef6eC433076C5e0BCbAFcC033537b48B0b',
     BSC_BRIDGE_CONTRACT_ADDRESS: '0x2700d71C5FCfd5109f6997Aaf6A6e1B47ED0E717',
-    ETH_EXPLORER: 'https://ropsten.etherscan.io',
+    ETH_EXPLORER: 'https://ropsten.etherscan.io', //https://etherscan.io/
     ETH_GLITCH_ADDRESS: '0xB176b2f01487a1A452e058f2b386a6eD30D38128',
     ETH_BRIDGE_CONTRACT_ADDRESS: '0xfeA109179cC904C5CBdD4E4d569e222f60487340',
     BACK_END_URL: 'https://bridge.glitch.finance'
