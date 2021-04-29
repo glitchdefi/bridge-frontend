d4bb438706217c321ce5d057b4416ef72cf5e2ab
Thoai Nguyen <nguyenthoai06011990@gmail.com>
2021-04-29 14:55:02 +0700

    update config mainnet

--- src/_configs/prod-config.js
+++ src/_configs/prod-config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    BLOCKCHAIN_NETWORK: 'TESTNET', // MAINNET
-    BSC_EXPLORER: 'https://testnet.bscscan.com',  //https://bscscan.com
+    BLOCKCHAIN_NETWORK: 'MAINNET', // MAINNET
+    BSC_EXPLORER: 'https://bscscan.com',  //https://bscscan.com
     BSC_GLITCH_ADDRESS: '0xF0902eB0049A4003793BAb33F3566A22D2834442',
-    BSC_BRIDGE_CONTRACT_ADDRESS: '0x48f845f74507d648aac9b723dc08fa9ae06224b5',
-    ETH_EXPLORER: 'https://ropsten.etherscan.io', //https://etherscan.io/
+    BSC_BRIDGE_CONTRACT_ADDRESS: '0x48F845f74507d648Aac9b723Dc08FA9Ae06224B5',
+    ETH_EXPLORER: 'https://etherscan.io', //https://etherscan.io
     ETH_GLITCH_ADDRESS: '0x038a68ff68c393373ec894015816e33ad41bd564',
-    ETH_BRIDGE_CONTRACT_ADDRESS: '0x48f845f74507d648aac9b723dc08fa9ae06224b5',
+    ETH_BRIDGE_CONTRACT_ADDRESS: '0x48F845f74507d648Aac9b723Dc08FA9Ae06224B5',
     BACK_END_URL: 'https://bridge.glitch.finance'
 };
