8e49c1ef4497007706fc5df024eaceb0581c32bb
minggltichfe <ming@glitch.finance>
2021-09-19 14:37:11 +0700

    Change bg jpg -> webp

--- src/app/layouts/Container.js
+++ src/app/layouts/Container.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background-image: url(/images/g_bg.jpg);
+  background-image: url(/images/g_bg.webp);
   background-size: cover;
   background-attachment: fixed;
 
