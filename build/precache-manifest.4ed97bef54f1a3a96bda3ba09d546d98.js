22bdc5390624222e7c912cdfa2cf334a3f7de5d0
minggltichfe <ming@glitch.finance>
2023-04-17 14:59:40 +0700

    feat: update build latest

---
+++ build/precache-manifest.4ed97bef54f1a3a96bda3ba09d546d98.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f906f6d6b3a1222ac3090a723057be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5440ac7f80b3c6c14986",
+    "url": "/static/css/2.bc12a319.chunk.css"
+  },
+  {
+    "revision": "e66a648cba699815fa50",
+    "url": "/static/css/main.5365ff9e.chunk.css"
+  },
+  {
+    "revision": "5440ac7f80b3c6c14986",
+    "url": "/static/js/2.bcfcce25.chunk.js"
+  },
+  {
+    "revision": "d20c3e3a5e4b6b2aec474d608371aff5",
+    "url": "/static/js/2.bcfcce25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e66a648cba699815fa50",
+    "url": "/static/js/main.1f98b526.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.1f98b526.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5627401265f3e5028d1",
+    "url": "/static/js/runtime-main.01a6e539.js"
+  }
+]);
